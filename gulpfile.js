c30a747e0c2a6d861bceb8377186bd11cefca07f
Cristian Sanchez <artcsch@outlook.com>
2016-04-13 20:55:17 -0500

    Actualizando proyecto con automaztizacion con gulp y utilizando materilazi, jquery, sass, npm y se crear los mudulos y las carpetas del proyecto

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
+
+var config ={
+	sass:{
+		main: './src/sass/main.scss',
+		watch: './src/sass/**/*.scss',
+		output: './build/css'
+	},
+	html:{
+		watch: './src/*.html'
+	}
+}
+gulp.task('server', function(){
+	gulp.src('./src')
+		.pipe(webserver({
+			host: '0.0.0.0',
+			port: 8080,
+			livereload: {
+				enable: true,
+				port: 35728
+			}
+
+		}));
+});
+
+gulp.task('build:css', function(){
+	gulp.src(config.sass.main)
+		.pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest(config.sass.output));
+});
+
+gulp.task('watch', function(){
+	gulp.watch(config.sass.watch, ['build:css']);
+	gulp.watch(config.html.watch, ['build']);
+	});
+
+gulp.task('build', ['build:css']);
+gulp.task('default', ['server', 'watch', 'build']);
